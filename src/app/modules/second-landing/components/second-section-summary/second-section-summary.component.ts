b5f59ff823178222f19644ea8faba445391d0ab2
Jramos <jramos@saludelectronica.com>
2022-12-08 13:10:59 -0500

    Se comienza landing gratuita.

---
+++ src/app/modules/second-landing/components/second-section-summary/second-section-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-section-summary',
+  templateUrl: './second-section-summary.component.html',
+  styleUrls: ['./second-section-summary.component.scss']
+})
+export class SecondSectionSummaryComponent {
+
+}
--- src/app/modules/second-landing/second-landing.module.ts
+++ src/app/modules/second-landing/second-landing.module.ts
@@ -4,16 +4,22 @@ import { CommonModule } from '@angular/common';
 import { SecondLandingRoutingModule } from './second-landing-routing.module';
 import { SecondLandingComponent } from './second-landing.component';
 import { SecondMainSectionComponent } from './components/second-main-section/second-main-section.component';
+import { SecondSectionStepsComponent } from './components/second-section-steps/second-section-steps.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { SecondSectionSummaryComponent } from './components/second-section-summary/second-section-summary.component';
 
 
 @NgModule({
   declarations: [
     SecondLandingComponent,
-    SecondMainSectionComponent
+    SecondMainSectionComponent,
+    SecondSectionStepsComponent,
+    SecondSectionSummaryComponent
   ],
   imports: [
     CommonModule,
-    SecondLandingRoutingModule
+    SecondLandingRoutingModule,
+    SharedModule
   ]
 })
 export class SecondLandingModule { }
