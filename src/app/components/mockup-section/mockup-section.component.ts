79e4692c22e39e6b50ab4577842e297536c68b03
Jramos <jramos@saludelectronica.com>
2022-12-07 23:26:29 -0500

    Se crean los demas componentes de la landing v-2

---
+++ src/app/components/mockup-section/mockup-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mockup-section',
+  templateUrl: './mockup-section.component.html',
+  styleUrls: ['./mockup-section.component.scss']
+})
+export class MockupSectionComponent {
+
+
+}
