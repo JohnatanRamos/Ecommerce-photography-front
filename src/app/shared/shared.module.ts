9705db60beedd39560ae631cbbf13561f40cd48e
Jramos <jramos@saludelectronica.com>
2022-12-07 23:25:25 -0500

    Se crea el componente del boton.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { BuyButtonComponent } from './components/buy-button/buy-button.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BuyButtonComponent
+  ],
   imports: [
     CommonModule,
     PrimeNgModule
+  ],
+  exports: [
+    BuyButtonComponent
   ]
 })
 export class SharedModule { }
