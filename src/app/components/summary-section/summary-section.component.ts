72845a23236cbc34611ed9c6d023da1c24cdaa3c
Jramos <jramos@saludelectronica.com>
2022-12-07 23:26:29 -0500

    Se crean los demas componentes de la landing v-2

---
+++ src/app/components/summary-section/summary-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-section',
+  templateUrl: './summary-section.component.html',
+  styleUrls: ['./summary-section.component.scss']
+})
+export class SummarySectionComponent {
+
+}
